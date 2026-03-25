8eb3023257b8565e399a2f0ff15fcfca19039fb5
coza_nostra7 <begzodmuminovb@gmail.com>
2026-03-25 09:50:05 +0100

    update 3

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -183,7 +183,7 @@ const testimonials: TTestimonial[] = [
 
 const projects: TProject[] = [
   {
-    name: "Car Rent",
+    name: "Hotel Management system",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
